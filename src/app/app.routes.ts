0b91415e079640604969929426b9a42b6154a192
Will-h0219 <wherrera0219@gmail.com>
2024-01-25 21:30:07 -0500

    set routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ export const routes: Routes = [
   {
     path: 'characters',
     title: 'Characters',
-    loadComponent: () => import('./features/character-list/character-list.component')
+    loadComponent: () => import('./features/character-list/character-list.component'),
+    data: { icon: 'person' }
   },
   {
     path: 'characters/:id',
@@ -14,12 +15,19 @@ export const routes: Routes = [
   {
     path: 'comics',
     title: 'Comics',
-    loadComponent: () => import('./features/comic-list/comic-list.component')
+    loadComponent: () => import('./features/comic-list/comic-list.component'),
+    data: { icon: 'book' }
   },
   {
     path: 'comics/:id',
     title: 'Comics',
     loadComponent: () => import('./features/comic/comic.component')
+  },
+  {
+    path: 'settings',
+    title: 'Settings',
+    loadComponent: () => import('./features/settings/settings.component'),
+    data: { icon: 'settings' }
   },
   {
     path: '**',
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,20 @@
-import { Component, Signal, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { onSidenavChange } from './layout.animations';
+import { routes } from '../app.routes';
+import { CommonModule } from '@angular/common';
+
+interface MenuItem {
+  path: string;
+  title: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-layout',
@@ -12,7 +23,9 @@ import { onSidenavChange } from './layout.animations';
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    CommonModule,
+    RouterModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
@@ -20,6 +33,14 @@ import { onSidenavChange } from './layout.animations';
 })
 export class LayoutComponent {
   sidenavState = signal(false);
+  routes: MenuItem[] = routes
+    .filter(route => route && route.path && route.path !== '**')
+    .filter(route => !route.path?.includes(':'))
+    .map(route => ({
+      path: route.path!,
+      title: route.title as string,
+      icon: route.data!['icon']
+    }));
 
   toggleSidenavState() {
     this.sidenavState.update(() => !this.sidenavState());
